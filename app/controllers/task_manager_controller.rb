692a87bb25a5cce5494f2d7b60bce747fcdc5f1c
Eduardo Sguerra <ed.sguerra@gmail.com>
2017-10-31 11:39:56 +0000

    rails new

---
+++ app/controllers/task_manager_controller.rb
@@ -0,0 +1,27 @@
+class TaskManagerController < ApplicationController
+  def list:
+  end
+
+  def string
+  end
+  def view:
+  end
+
+  def string
+  end
+  def add:
+  end
+
+  def string
+  end
+  def edit:
+  end
+
+  def string
+  end
+  def remove:
+  end
+
+  def string
+  end
+end
